cd7e69b8947f4f7d1f8677e68e21f3bcd205116c
subin-code <85931238+subin-code@users.noreply.github.com>
2021-06-30 09:10:37 +0900

    Add files via upload

---
+++ muo_test/src/main/java/com/springbook/domain/findPWDTO.java
@@ -0,0 +1,18 @@
+package com.springbook.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class findPWDTO {
+	private String findPWType;
+	private String mb_id;
+	private String mb_name;
+	private String mb_phone;
+	private String mb_email;
+	
+	
+}
