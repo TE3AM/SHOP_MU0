9677301a6ecbf8efb2e2dd23ee2c5596d4f2ad75
subin-code <85931238+subin-code@users.noreply.github.com>
2021-06-30 09:12:44 +0900

    Add files via upload

--- muo_test/src/main/java/com/springbook/mapper/MemberMapper.java
+++ muo_test/src/main/java/com/springbook/mapper/MemberMapper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import com.springbook.domain.Criteria;
 import com.springbook.domain.MemberVO;
+import com.springbook.domain.findPWDTO;
 
 public interface MemberMapper {
 	public void register(MemberVO vo);
@@ -12,6 +13,11 @@ public interface MemberMapper {
 	public int update(MemberVO vo);
 	public List<MemberVO> getList();
 	public List<MemberVO> getListWithPaging(Criteria cri);
-	int getTotalCount(Criteria cri);
-	public String findPW(MemberVO vo);
+	public int getTotalCount(Criteria cri);
+	public String findPW(findPWDTO type);
+	public int checkIdValidation(String mb_id);
+	public int checkPhoneValidation(String mb_phone);
+	public MemberVO getUpdateInfo(String mb_id);
+	public int updateByUser(MemberVO member);
+	
 }
